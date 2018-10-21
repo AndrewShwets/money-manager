2d9c22a4e5f19d195a9f63d0eb2fbf852cc0ef90
andrewshwets <andrewshwets@gmail.com>
2018-10-21 22:09:30 +0300

    added menu underline decoration

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,25 +9,81 @@ import { NAVIGATION_ROUTES } from 'routes';
 
 import './index.scss';
 
-const { string } = PropTypes;
+const { string, shape } = PropTypes;
+
+const getElemCoord = elem => elem && elem.getBoundingClientRect();
 
 class Navigation extends PureComponent {
     static propTypes = {
         className: string,
+        location: shape({
+            pathname: string
+        }).isRequired,
     }
 
     static defaultProps = {
         className: '',
     }
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menuItemStyle: {},
+        };
+
+        this._menuItems = {};
+        this._nav = null;
+    }
+
+    componentDidMount() {
+        this.updateMenuStyle();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { location } = this.props;
+
+        if (prevProps.location.pathname !== location.pathname) {
+            this.updateMenuStyle();
+        }
+    }
+
+    /**
+     * Calculates nav coordinates and selected menu item,
+     * for updating underline styles
+     */
+    updateMenuStyle = () => {
+        const { location } = this.props;
+
+        const navCoord = getElemCoord(this._nav);
+        const selectedItemCoord = getElemCoord(this._menuItems[location.pathname]);
+
+        this.setState({
+            menuItemStyle: {
+                left: `${selectedItemCoord.left - navCoord.left + 10}px`,
+                right: `${navCoord.right - selectedItemCoord.right + 10}px`,
+            },
+        })
+    }
+
     render() {
         const { className } = this.props;
+        const { menuItemStyle } = this.state;
 
         return (
             <nav className={classNames('nav', className)}>
-                <ul className="flex nav__list">
+                <ul
+                    ref={(nav) => this._nav = nav}
+                    className="flex nav__list"
+                >
                     {NAVIGATION_ROUTES.map(({ title, path, exac }, index) => (
-                        <li className="nav__list-item" key={path}>
+                        <li
+                            ref={(item) => (
+                                !Boolean(this._menuItems[path]) && (this._menuItems[path] = item)
+                            )}
+                            className="nav__list-item"
+                            key={path}
+                        >
                             <NavLink
                                 to={path}
                                 exac={exac}
@@ -36,6 +92,10 @@ class Navigation extends PureComponent {
                             </NavLink>
                         </li>
                     ))}
+                    <li
+                        className="nav__underline"
+                        style={menuItemStyle}
+                    />
                 </ul>
             </nav>
         );
