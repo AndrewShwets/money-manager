e38f53bfb67bcba60bec081bd3d74634bf261261
AndrewShwets <AndrewShwets@gmail.com>
2019-03-26 23:40:27 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 
 import initAppLocalStorage from 'localStorage';
-
 import store from 'store';
 
 import 'normalize.css';
